a56ee2a2fa841f76dda6b3bc0f4388c0481fc8dd
Nevin-Bali100 <opopopopop100000@gmail.com>
2025-06-21 19:47:46 +0530

    Completing And Debugging

--- FrontEnd/src/components/Spinner.jsx
+++ FrontEnd/src/components/Spinner.jsx
@@ -1,6 +1,6 @@
 const Spinner = () => {
   return (
-    <div className="animate-ping w-16 h-16 m-8 rounded-full bg-blue-600"></div>
+    <div className="animate-ping w-16 h-16 m-8 rounded-full bg-blue-600 flex justify-center items-center"></div>
   );
 };
 
